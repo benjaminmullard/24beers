8ae0fce01d7527e06afa6f7bef29f5f0966c247d
Ben Mullard <ben@easynomad.co.uk>
2015-04-14 22:35:51 +0100

    Setting up Commerce project

--- src/main/java/uk/co/easynomad/commerce/server/CommerceState.java
+++ src/main/java/uk/co/easynomad/commerce/server/CommerceState.java
@@ -2,13 +2,32 @@ package uk.co.easynomad.commerce.server;
 
 public enum CommerceState
 {
-
-    INITIAL {
-        public CommerceState nextState()
+    INITIAL
+    {
+        @Override
+        public CommerceState getNextState()
         {
+            final CommerceState nextState;
 
+            nextState = null;
+
+            return nextState;
         }
-    }
+    },
+    BROWSING_NO_PERSONALISATION
+    {
+        @Override
+        public CommerceState getNextState() {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+    },
+    BROWSING_WITH_PERSONALISATION
+    {
+        @Override
+        public CommerceState getNextState() {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+    };
 
-    public abstract CommerceState nextState();
+    public abstract CommerceState getNextState();
 }
